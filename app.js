ef4355cef2470711bdbc8031e82b7599371f6e68
kev1226 <kevin_alt12@hotmail.com>
2024-11-26 16:47:35 -0500

    Actualizacion

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 8082;
+const port = 8080;
 
+// Ruta principal que responde con un mensaje
 app.get('/', (req, res) => {
   res.send('¡PAGINA WEB EN EC2 kevin!');
 });
 
+// Iniciar el servidor en el puerto 80 (puerto estándar para HTTP)
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
